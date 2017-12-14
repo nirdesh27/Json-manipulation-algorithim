1d11dfc5cb62df5e6f8d11e227081e3a9790406f
nirdesh27 <nirdesh27@rediffmail.com>
2017-12-14 14:06:39 +0530

    first commit of javascript_recursion code

---
+++ recursion_javaScript.js
@@ -0,0 +1,46 @@
+ function createHashMapFromJsonString(jsonData, parentkey) {
+            var localkey = "";
+            for (var key in jsonData) {
+                if (jsonData.hasOwnProperty(key)) {
+                    //console.log(key,"======");
+                    if (jsonData[key] instanceof Array) {
+                        localkey += key;
+
+                        for (var i = 0; i < jsonData[key].length; i++) {
+                            createHashMapFromJsonString(jsonData[key][i], parentkey + localkey + ":");
+                        }
+                    } else if (jsonData[key] instanceof Object) {
+                        localkey += key + "";
+                        //console.log(jsonData[key]);
+                        createHashMapFromJsonString(jsonData[key], parentkey + localkey + ":");
+                    } else {
+                        //console.log('34234234rewerwe',parentkey,'---',key,'___---',jsonData);
+                        //console.log(parentkey+key,"===",jsonData[key])
+
+                        var value = jsonData[key];
+                        var hash_value;
+                        //console.log(parentkey+key,"+++");
+                        if (arrayToBeMatched.includes((parentkey + key))) {
+                            hash_value = MD5(value.toString());
+                            //dict[parentkey + keyword + key] = hash_value + "____" + jsonData[key];
+                            jsonData[keyword + key] = hash_value + "____" + jsonData[key];
+                            var old_key = key;
+                            var new_key = keyword + key;
+                            // jsonData[i] = translated_response_dict[j];
+                            Object.defineProperty(jsonData, old_key, Object.getOwnPropertyDescriptor(jsonData, old_key));
+                            delete jsonData[old_key];
+                            //console.log(jsonData[keyword+key],"====");
+                            arrayToBeTranslated.push(hash_value);
+                        } else {
+                            //dict[parentkey + key] = value.toString();
+                            continue;
+                            //jsonData[keyword+key]=jsonData[key];
+                        }
+                        //array.push(parentkey+key);
+
+                    }
+                }
+                localkey = "";
+            }
+
+        }
